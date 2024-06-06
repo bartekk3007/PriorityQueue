588b50431f74f88e1efa2bf34821d9efe66ecf39
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-06 23:49:06 +0200

    Add files via upload

---
+++ priority_queue_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "priority_queue_list.h"
+
+void qlist(pqueue *head, void (*print_data)(void *)) {
+	pqueue *p;
+	
+	for (p = head; p != NULL; p = p->next) {
+		printf("%d: ", p->k);
+		print_data(p->data);
+		printf("\n");
+	}
+	
+}
+
+void qinsert(pqueue **phead, void *data, int k) {
+}
+
+
+void qremove(pqueue **phead, int k) {
+}
+
